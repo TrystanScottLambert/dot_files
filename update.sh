3855e90f4f453bd97dd3f96ed91c466c580e6e06
TrystanScottLambert <trystanscottlambert@gmail.com>
2026-06-29 17:07:18 +0800

    updating

---
+++ update.sh
@@ -0,0 +1,16 @@
+#Script to copy all the current dot files
+
+# bash profile
+cp ~/.bashrc bash/bashrc
+
+# neovim
+cp ~/.config/nvim/ nvim/
+
+# starship
+cp ~/.config/starship.toml starship/
+
+# ghostty
+cp ~/.config/ghostty/* ghostty/
+
+git add .
+git commit -m "updating"
